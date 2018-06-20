a7d5e65e1bb65c669e842d0598e74b94bc0ab8b1
Katie Park <katie.e.park@gmail.com>
2018-06-20 14:05:35 -0400

    Update gulpfile to watch for changes in lib js

--- gulpfile.js
+++ gulpfile.js
@@ -190,7 +190,7 @@ const buildProduction = gulp.series(clean, productionStyles, productionScripts, 
 function watch() {
   gulp.watch(['README.md'], readme);
   gulp.watch(['src/*.scss'], styles);
-  gulp.watch(['src/*.js'], scripts);
+  gulp.watch(['src/*.js', 'src/lib/*.js'], scripts);
   gulp.watch(['src/assets/**'], assets);
   // Triggers a full refresh (html doesn't actually need to be recompiled)
   gulp.watch(['post-templates/**'], html);
