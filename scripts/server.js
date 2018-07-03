542be5429b6a25d591e479c59deef574ad0c12d8
Gabe Isman <gabe.isman@gmail.com>
2018-07-02 22:54:55 -0400

    Simplify local static assets.

--- scripts/server.js
+++ scripts/server.js
@@ -5,14 +5,11 @@ var renderer = require('./localrenderer.js');
 module.exports = function(options) {
   var app = express();
   app.use(express.static('build'));
+  app.use(express.static('post-templates'));
 
   var config;
   config = JSON.parse(fs.readFileSync('./config.json'), 'utf-8');
   console.log('Config:', config);
-
-  // TODO is this really the best way to serve fonts?
-  var fonts = fs.readFileSync('./post-templates/fonts.css', 'utf-8');
-  var ghMarkdown = fs.readFileSync('./post-templates/github-markdown.css', 'utf-8');
 
   var lrPort = options.lrPort || 35729;
 
@@ -22,18 +19,6 @@ module.exports = function(options) {
 
   app.get('/readme/', function(req, res) {
     res.send(renderer.renderReadme({ lrPort: lrPort }))
-  });
-
-  app.get('/fonts.css', function(req, res) {
-    res.contentType('text/css');
-    res.setHeader('Cache-Control', 'public,max-age=60')
-    res.send(fonts);
-  });
-
-  app.get('/github-markdown.css', function(req, res) {
-    res.contentType('text/css');
-    res.setHeader('Cache-Control', 'public,max-age=60')
-    res.send(ghMarkdown);
   });
 
   port = options.port || 3000;
