a80f7497ccfd999d1a0fd9cd116166e3f8eaab3f
Gabe Isman <gabe.isman@gmail.com>
2018-08-08 18:02:13 -0400

    Working transcoding with mux, no creds, no output caching.

--- scripts/videos.js
+++ scripts/videos.js
@@ -1,37 +1,8 @@
 const credentials = require('./credentials.js');
-const AWS = require('aws-sdk');
 const fs = require('fs');
 const urljoin = require('url-join');
 const config = require('../config.json');
-
-
-function configureAWSClient(done) {
-  credentials.ensureCredentials(function(creds) {
-    AWS.config.update({
-      region: 'us-east-1',
-      accessKeyId: creds['gfx-aws-access'],
-      secretAccessKey: creds['gfx-aws-secret']
-    });
-    configureMediaConvertEndpoint(done);
-  });
-}
-
-
-function configureMediaConvertEndpoint(done) {
-  // Create empty request parameters
-  const params = {
-    MaxResults: 0,
-  };
-
-  new AWS.MediaConvert()
-    .describeEndpoints(params).promise()
-    .then((value) => {
-      AWS.config.mediaconvert = { endpoint: value.Endpoints[0].Url };
-      done();
-    }, (error) => {
-      throw new Error('Error Getting MediaConvert endpoint: ' + error);
-    });
-}
+const axios = require('axios');
 
 
 function getVideoUrls() {
@@ -40,57 +11,57 @@ function getVideoUrls() {
     if (!file.match(/.mp4$/)) {
       return;
     }
-    results.push(getS3Url(file));
+    results.push(getCDNUrl(file));
   });
   return results;
 }
 
 
-function getS3Url(filename) {
-  return 's3://' + urljoin(config.bucket, config.slug, 'assets', filename);
+function getCDNUrl(filename) {
+  return urljoin(config.cdn, config.slug, 'assets', filename);
 }
 
 
-function getS3OutputBucket() {
-  return 's3://' + urljoin(config.bucket, config.slug, 'assets', 'converted');
-}
-
-
-function getJobConfig(url) {
-  const jobConfig = JSON.parse(fs.readFileSync('./scripts/video-default.json'));
-  jobConfig.Settings.Inputs[0].FileInput = url;
-  jobConfig.Settings.OutputGroups = jobConfig.Settings.OutputGroups.map((group) => {
-    group.OutputGroupSettings.HlsGroupSettings.Destination = getS3OutputBucket();
-    return group;
-  });
-  return jobConfig;
+function getTranscodingParams(url) {
+  return {
+    playback_policy: 'public',
+    per_title_encode: true, // https://docs.mux.com/docs/per-title-encoding
+    input: url
+  };
 }
 
 
 function transcodeAll() {
-  configureAWSClient(() => {
-    getVideoUrls().forEach((url) => {
-      new AWS.MediaConvert().createJob(getJobConfig(url)).promise()
-        .then((data) => {
-          console.log('Transcoding job successfully created', data);
-        }, (err) => {
-          console.log('Error creating transcoding job', err);
-        });
-    })
-  });
-}
-
-
-function listRecentJobs() {
-  configureAWSClient(() => {
-    new AWS.MediaConvert().listJobs().promise()
-      .then((data) => {
-        console.log(data);
-      }, (err) => {
-        console.log(err);
+  const promises = getVideoUrls().map((url) => {
+    return axios.post('https://api.mux.com/video/v1/assets',
+      getTranscodingParams(url),
+      {
+        auth: {
+          // TODO implement credentials
+          username: '',
+          password: '',
+        }
+      })
+      .then((res) => {
+        return formatOutput(url, res.data);
+      })
+      .catch((error) => {
+        return error.message;
       });
   });
+  return Promise.all(promises).then((outputs) => {
+    return outputs.reduce((accumulator, value) => {
+      return Object.assign(accumulator, value);
+    }, {});
+  });
 }
 
-// transcodeAll();
-listRecentJobs();
+
+function formatOutput(url, responseData) {
+  return {
+    [url]: `https://stream.mux.com/${responseData.data.playback_ids[0].id}.m3u8`,
+  };
+}
+
+
+transcodeAll().then(console.log);
