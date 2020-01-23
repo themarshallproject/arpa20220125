5a1077e9aeacfbd6db594bc1218d2595acb70a1b
Katie Park <katie.e.park@gmail.com>
2020-01-23 17:36:31 -0500

    Remove test examples

--- examples/test-1/graphic.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(() => {
-  const $test1 = $('#test-graphic-1');
-  $test1.css('background', 'gold');
-});
-
--- examples/test-1/lib/test-lib.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(() => {
-  $('body').css('background', 'lightcyan');
-});
