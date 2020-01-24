ac9eb22b15d369b9983e61f2473ba5ade3fa9904
Katie Park <katie.e.park@gmail.com>
2020-01-24 12:09:04 -0500

    Moving sidebar charts into examples rig

--- examples/bar-charts/graphic.js
+++ examples/bar-charts/graphic.js
@@ -1,8 +1,12 @@
 import * as utilities from 'charts/utilities.js';
 import NationalBarChart from './national-bar-chart.js'
+import StackedBarChart from './stacked-bar.js'
+import PaymentChart from './payment-chart.js'
 
 $(document).ready(() => {
   initNationalBar();
+  initSidebarVertical();
+  initSidebarHorizontal();
 });
 
 function initNationalBar() {
@@ -20,5 +24,36 @@ function initNationalBar() {
     marginBottom: 30,
     marginTop: 15,
     xAxisTickFormat: (d, width) => { return width < mobileBreak ? utilities.abbrevMonth(d) : d},
+  })
+}
+
+function initSidebarVertical() {
+  const barChartMC = new StackedBarChart({
+    containerId: 'dd-chart-one',
+    data: CASE_DATA,
+    bandKey: 'year',
+    valueKey: 'case',
+    barPadding: 0.4,
+    marginLeft: 30,
+    marginRight: 0,
+    roundedYMax: 500,
+    yAxisTicks: 5
+  });
+}
+
+function initSidebarHorizontal() {
+  const paymentChart = new PaymentChart({
+    containerId: 'dd-chart-two',
+    data: PAYMENT_DATA,
+    bandKey: "event",
+    valueKey: "pay",
+    marginLeft: 80,
+    marginRight: 30,
+    marginBottom: 0,
+    roundedXMax: 200,
+    barPadding: 0.3,
+    labelFormat: (d) => {return d == 180 ? `$${ d }` : d
+  },
+    marginTop: 0
   })
 }
