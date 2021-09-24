78d772fd25375329d409dd38f57890bdb845199b
Katie Park <katie.e.park@gmail.com>
2021-09-24 11:48:15 -0400

    Use gulp-transform to generate embed loader files

--- scripts/external-embeds.js
+++ scripts/external-embeds.js
@@ -1,33 +1,25 @@
 const fs = require('fs');
-const glob = require('glob');
-const path = require('path');
+const gulp = require('gulp');
+const transform = require('gulp-transform');
 
 const config = require('../config.json');
 const loaderScriptVersion = '0.1.11';
 
 function embedLoaderHtml(cb) {
-  const graphicPaths = glob.sync('src/*.html');
-
-  for (i in graphicPaths) {
-    const filename = path.basename(graphicPaths[i], '.html');
-    const embedLoaders = renderEmbed(filename);
-    const loadersPath = './embed-loaders';
-    fs.mkdirSync(loadersPath, { recursive: true });
-    fs.writeFileSync(`${ loadersPath }/embed-${ filename }.html`, embedLoaders);
-  }
-
-  cb();
+  return gulp.src('src/*.html')
+    .pipe(transform('utf-8', renderEmbed))
+    .pipe(gulp.dest('build/embed-loaders'));
 }
 
 
-function renderEmbed(filename) {
-  const embedId = `g-tmp-embed-${ config.slug }-${ filename }`;
+function renderEmbed(contents, file) {
+  const embedId = `g-tmp-embed-${ config.slug }-${ file.stem }`;
   return `<div id="${ embedId }" data-tmp-slug="${ config.slug }"></div>
 <script type="text/javascript" src="${ config.cdn }/tmp-gfx-embed-loader/loader-${ loaderScriptVersion }.js"></script>
 <script type="text/javascript">
   var tmpEmbed = new TMPGraphicEmbed({
     id: '${ embedId }',
-    graphicPath: 'embed-contents/${ filename }.html',
+    graphicPath: 'embed-contents/${ file.basename }',
     baseUrl: '${ config.cdn }/${ config.slug }',
     });
 </script>`;
