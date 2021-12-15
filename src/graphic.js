8dae0057518d5593475821701c1b3f13fbf48ce3
Ryan Murphy <rdmurphy@users.noreply.github.com>
2021-12-15 15:14:08 -0600

    No longer encourage this

--- src/graphic.js
+++ src/graphic.js
@@ -1,6 +1,2 @@
 // Uncomment this to use js utilities
 //import * as utilities from 'charts/utilities.js';
-
-// Wait for document ready before executing code that interacts with the DOM
-$(document).ready(() => {
-});
