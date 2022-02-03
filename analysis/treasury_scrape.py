2039b3416a66fb7b5d50339417a62967bd66fbe1
Anastasia Valeeva <anastasiya.valeeva@gmail.com>
2022-02-03 20:57:58 +0300

    left with f string to fix

--- analysis/treasury_scrape.py
+++ analysis/treasury_scrape.py
@@ -7,38 +7,41 @@ import pandas as pd
 @click.argument("output_file", type=click.File('w'))
 def treasury_scrape(output_file):
   
-  response = requests.get("https://home.treasury.gov/policy-issues/coronavirus/assistance-for-state-local-and-tribal-governments/state-and-local-fiscal-recovery-funds/recovery-plan-performance-reports-2021")
-  soup = BeautifulSoup(response.text, 'html.parser')
+    response = requests.get("https://home.treasury.gov/policy-issues/coronavirus/assistance-for-state-local-and-tribal-governments/state-and-local-fiscal-recovery-funds/recovery-plan-performance-reports-2021")
+    soup = BeautifulSoup(response.text, 'html.parser')
   
-  table = soup.find_all('table')[0]
+    table = soup.find_all('table')[0]
   
-  ths = table.find_all('th')
-  headers=['Index']
-  for th in ths[1:]:
-    headers.append(th.text.strip())
+    ths = table.find_all('th')
+    headers=['Index']
+    for th in ths[1:]:
+        headers.append(th.text.strip())
 
-  tbody = table.find('tbody')
-  trs = tbody.find_all('tr')
-  table_list = []
-  for tr in trs:
-    tds = tr.find_all('td')
-    td_list = []
-    for (i, td) in enumerate (tds):
-        if (i == 5):
-            td_list.append(td.find('a')['href'])
-        elif (i== 7):
-            try:
+    tbody = table.find('tbody')
+    trs = tbody.find_all('tr')
+    table_list = []
+    for tr in trs:
+        tds = tr.find_all('td')
+        td_list = []
+        for (i, td) in enumerate (tds):
+            if (i == 5):
                 td_list.append(td.find('a')['href'])
-            except:
-                pass
-        else:
-            td_list.append(td.text)
+            elif (i== 7):
+                try:
+                    td_list.append(td.find('a')['href'])
+                except:
+                    pass
+            else:
+                td_list.append(td.text)
 
-    click.echo(f'scraped {td_list[1]}', err=True)
-    table_list.append(td_list)
+        click.echo(f'scraped {td_list[1]}', err=True)
+        table_list.append(td_list)
 
     df = pd.DataFrame(table_list, columns = headers)
-    df['Recovery Plan'] = (f'https://home.treasury.gov{df["Recovery Plan"]}')
+    #df.rename(columns={"Recovery Plan": "Recovery_Plan"}, inplace=True)
+    #df["Recovery_Plan"] =  (f'https://home.treasury.gov{df["Recovery Plan"]}')
+    df['Recovery Plan'] = ('https://home.treasury.gov' + df['Recovery Plan'])
+    #df["Recovery_Plan_new"] = (f"https://home.treasury.gov{df.Recovery_Plan}")
     df.to_csv(output_file, index=False)
 
 if __name__ == '__main__':
