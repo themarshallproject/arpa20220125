779dd1e50300af83d63216c7810cf35a87bb8000
Anastasia Valeeva <anastasiya.valeeva@gmail.com>
2022-02-09 19:05:15 +0300

    clean files

--- analysis/clean_interim_reports.py
+++ analysis/clean_interim_reports.py
@@ -42,23 +42,3 @@ for file in filenames:
     name = name.split(',')[0]
     df['Territory'] = name
     df.to_excel(f'output_data/CLEAN/{name}.xls', index = False)
-
-
-x = 'output_data/AL/Randolph County, Alabama Interim Report.xls'
-
-parse(x, excelHandler)
-df = pd.DataFrame(excelHandler.tables[0][7:], columns=excelHandler.tables[0][6])
-
-df = df[df["Category"].notna()]
-df = df[df["Category"].str.contains("Category") == False]
-df = df[df["Cumulative Obligations"].str.contains("Total Cumulative Obligations") == False]
-df.drop(df.tail(1).index,inplace=True)
-df.drop([86], inplace=True)
-name = x.split("AL/")[1]
-name = name.split('.')[0]
-name = name.split(',')[0]
-df['Territory']= name
-df.to_excel(f'output_data/NEW/{name}.xls', index = False)
-
-
-
