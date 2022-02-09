d4b1530c62d0f699c2dc65a65e26904d55bf0ba7
Anastasia Valeeva <anastasiya.valeeva@gmail.com>
2022-02-09 17:33:49 +0300

    draft cleaning script + pipenv installs

---
+++ analysis/clean_interim_reports.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import glob
+from xml.sax import ContentHandler, parse
+
+
+filenames = glob.glob("output_data/AL/*.xls")
+
+class ExcelHandler(ContentHandler):
+    def __init__(self):
+        self.chars = [  ]
+        self.cells = [  ]
+        self.rows = [  ]
+        self.tables = [  ]
+    def characters(self, content):
+        self.chars.append(content)
+    def startElement(self, name, atts):
+        if name=="Cell":
+            self.chars = [  ]
+        elif name=="Row":
+            self.cells=[  ]
+        elif name=="Table":
+            self.rows = [  ]
+    def endElement(self, name):
+        if name=="Cell":
+            self.cells.append(''.join(self.chars))
+        elif name=="Row":
+            self.rows.append(self.cells)
+        elif name=="Table":
+            self.tables.append(self.rows)
+
+excelHandler = ExcelHandler()
+
+for file in filenames:
+    parse(file, excelHandler)
+    df = pd.DataFrame(excelHandler.tables[0][7:], columns=excelHandler.tables[0][6])
+    df = df[df["Category"].notna()]
+    df = df[df["Category"].str.contains("Category") == False]
+    df = df[df["Cumulative Obligations"].str.contains("Total Cumulative Obligations") == False]
+    df.drop(df.tail(1).index,inplace=True) 
+    df.drop([86], inplace=True)
+    name = file.split("AL/")[1]
+    name = name.split('.xls')[0]
+    name = name.split(',')[0]
+    df['Territory'] = name
+    df.to_excel(f'output_data/CLEAN/{name}.xls', index = False)
+
+
+x = 'output_data/AL/Randolph County, Alabama Interim Report.xls'
+
+parse(x, excelHandler)
+df = pd.DataFrame(excelHandler.tables[0][7:], columns=excelHandler.tables[0][6])
+
+df = df[df["Category"].notna()]
+df = df[df["Category"].str.contains("Category") == False]
+df = df[df["Cumulative Obligations"].str.contains("Total Cumulative Obligations") == False]
+df.drop(df.tail(1).index,inplace=True)
+df.drop([86], inplace=True)
+name = x.split("AL/")[1]
+name = name.split('.')[0]
+name = name.split(',')[0]
+df['Territory']= name
+df.to_excel(f'output_data/NEW/{name}.xls', index = False)
+
+
+
