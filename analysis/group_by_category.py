4e36b4fc26ee783a08fb82e5a71b6a7687cf8dc2
Weihua Li <wli@themarshallproject.org>
2022-08-12 13:18:12 -0400

    Set up click

--- analysis/group_by_category.py
+++ analysis/group_by_category.py
@@ -1,5 +1,17 @@
-def process():
+import pandas as pd
+import click
+
+@click.command()
+@click.argument('input_filename')
+@click.argument('output_filename')
+def process(input_filename, output_filename):
     print("Hello World!")
+    print(input_filename)
+# @click.command()
+# @click.argument('input_filename')
+# def read_csv(input_filename):
+
+# def export(output_filename):
 
 if __name__ == '__main__':
     process()
