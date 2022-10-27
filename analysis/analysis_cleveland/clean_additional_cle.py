06d7fcc003278f5436b401ba3863a28bea5da920
anastasiajourno <anastasiya.valeeva@gmail.com>
2022-10-26 21:12:26 -0400

    clean additional spendings and spit out clean csv

---
+++ analysis/analysis_cleveland/clean_additional_cle.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.concat(pd.read_excel('analysis/source_data/cleveland/arpa_cle_additional.xlsx', sheet_name=None), ignore_index=True)
+
+#df['Amount'] = df['Amount'].str.replace(',','')
+df['Amount'] = df['Amount'].astype(float)
+
+df.to_csv("analysis/source_data/cleveland/additional_cle.csv", index = False)
