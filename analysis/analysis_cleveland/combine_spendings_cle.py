d33a0a19dd8a6fa4a740501933eb3a99884f8d92
anastasiajourno <anastasiya.valeeva@gmail.com>
2022-10-28 15:40:49 -0400

    fixes naming and location output file indirect cj

--- analysis/analysis_cleveland/combine_spendings_cle.py
+++ analysis/analysis_cleveland/combine_spendings_cle.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-#CLEANING THE INITIAL CJ SPENDING DATA
+#CLEANING THE DIRECT CJ SPENDING DATA
 df_init = pd.read_csv("analysis/source_data/cleveland/cuyahoga_arpa_projects_to_vet.csv")
 
 df_init = df_init[df_init['Vet'].str.contains("cj")==True]
@@ -18,21 +18,18 @@ df_init['Amount'] = df_init['Amount'].fillna(0)
 
 df_init['Amount'] = df_init['Amount'].astype(float)
 
-#CLEANING THE ADDITIONAL CJ SPENDING DATA
+#CLEANING THE INDIRECT CJ SPENDING DATA
 
-df_add = pd.read_csv("analysis/source_data/cleveland/arpa_cle_additional.csv")
+df_add = pd.read_csv("analysis/source_data/cleveland/arpa_cle_indirect_clean.csv")
 
 df_add = df_add[["recipient", "Amount", "Project description", "Focus"]]
 
 df_add["source"] = "indirect"
 
-#df_add['Amount'] = df_add['Amount'].str.replace('.00','')
-#df_add['Amount'] = df_add['Amount'].fillna(0)
 df_add['Amount'] = df_add['Amount'].astype(float)
-df_add.info()
 
 #CONCAT two files that have the same structure 
 
 df_all = pd.concat([df_init, df_add])
 
-df_all.to_csv("analysis/output_data/cle_cj_spending_data.csv", index=False) 
+df_all.to_csv("analysis/output_data/cle_cj_all_spending.csv", index=False) 
