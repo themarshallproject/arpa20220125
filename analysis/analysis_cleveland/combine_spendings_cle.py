daa19437eacfa0b8f528cc085e7242efb853d2ec
anastasiajourno <anastasiya.valeeva@gmail.com>
2022-10-31 17:04:15 -0400

    just come more file & naming cleaning

--- analysis/analysis_cleveland/combine_spendings_cle.py
+++ analysis/analysis_cleveland/combine_spendings_cle.py
@@ -1,35 +1,35 @@
 import pandas as pd
 
 #CLEANING THE DIRECT CJ SPENDING DATA
-df_init = pd.read_csv("analysis/source_data/cleveland/cuyahoga_arpa_projects_to_vet.csv")
+df_dir = pd.read_csv("analysis/source_data/cleveland/cuyahoga_arpa_projects_vetted.csv")
 
-df_init = df_init[df_init['Vet'].str.contains("cj")==True]
+df_dir = df_dir[df_dir['Vet'].str.contains("cj")==True]
 
-df_init = df_init[["Recipient Name", "Total Cumulative Obligations", "Project Description", \
+df_dir = df_dir[["Recipient Name", "Total Cumulative Obligations", "Project Description", \
     "Focus" ]].rename(columns = {
      "Recipient Name": "recipient",
      "Project Description": "Project description",
      "Total Cumulative Obligations": "Amount",
      "Focus": "Focus" })
 
-df_init["source"] = "direct"
+df_dir["source"] = "direct"
 
-df_init['Amount'] = df_init['Amount'].fillna(0)
+df_dir['Amount'] = df_dir['Amount'].fillna(0)
 
-df_init['Amount'] = df_init['Amount'].astype(float)
+df_dir['Amount'] = df_dir['Amount'].astype(float)
 
 #CLEANING THE INDIRECT CJ SPENDING DATA
 
-df_add = pd.read_csv("analysis/source_data/cleveland/cle_indirect_multiple.csv")
+df_indir = pd.read_csv("analysis/source_data/cleveland/cle_indirect_multiple.csv")
 
-df_add = df_add[["recipient", "Amount", "Project description", "Focus"]]
+df_indir = df_indir[["recipient", "Amount", "Project description", "Focus"]]
 
-df_add["source"] = "indirect"
+df_indir["source"] = "indirect"
 
-df_add['Amount'] = df_add['Amount'].astype(float)
+df_indir['Amount'] = df_indir['Amount'].astype(float)
 
 #CONCAT two files that have the same structure 
 
-df_all = pd.concat([df_init, df_add])
+df_all = pd.concat([df_dir, df_indir])
 
 df_all.to_csv("analysis/output_data/cle_cj_all_spending.csv", index=False) 
